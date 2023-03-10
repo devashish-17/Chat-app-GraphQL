aedb3a4fa57136935acc19e5603a1bab3afda7f7
Devashish Revadkar <ddrevadkar@gmail.com>
2023-03-10 20:43:59 +0530

    commit

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   console.log("data is = ", data);
 
   return (
-    <div className="">
+    <div className="bg-gray-400 h-screen">
       <Head>
         <title>Next Messenger</title>
         <link rel="icon" href="/favicon.ico" />
@@ -19,18 +19,15 @@ const Home: NextPage = () => {
         <h2> Next ChapApp </h2>
 
 
-        {data?.user ? (
-          <button>logout</button>
-        ) : (
-          <button
-            type="submit"
-            className='border-2 border-gray-400 h-10 w-60 rounded-md px-2 m-2'
-            onClick={() => signIn()}
-          >
-            Login
-          </button>
-        )}
+        <button
+          onClick={() => {
+            signIn();
+          }}
+        >
+          Login
+        </button>
 
+        hello world
 
 
       </main>
